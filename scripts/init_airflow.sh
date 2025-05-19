4fd4470c7b2b967a738faced7c090cdb32cf9f94
Muhammad Ihsan <muhihsan0@outlook.com>
2025-05-19 23:22:10 +0700

    finding out how to configure airflow user in 3.0.1

--- scripts/init_airflow.sh
+++ scripts/init_airflow.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-airflow db init
-
-airflow users create \
-	-- username airflow \
-	-- password airflow \
-	-- firstname Muhammad \
-	-- lastname Ihsan \
-	-- role Admin \
-	-- email muhihsan0.jkt@gmail.com
-
-tail -f /dev/null
+airflow db migrate
