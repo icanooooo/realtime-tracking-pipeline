0c1ec14735a3d24b62c687f4909fa275778d8b53
Muhammad Ihsan <muhihsan0@outlook.com>
2025-05-23 20:51:35 +0700

    succesfully create pipeline to update orders

--- scripts/update_orders.py
+++ scripts/update_orders.py
@@ -2,13 +2,13 @@ from helper.to_sql import to_sql, get_sql
 from helper.update_helpers import update_orders
 
 def update_data():  
-    a = get_sql("orders_history", "localhost")
-    b = get_sql("destinations", "localhost")
-    c = get_sql("station", "localhost")
+    a = get_sql("orders_history")
+    b = get_sql("destinations")
+    c = get_sql("station")
 
     df = update_orders(a, b, c)
     
-    to_sql(df, "orders_history", "localhost") 
+    to_sql(df, "orders_history") 
 
 if __name__ == "__main__":
     update_data()
