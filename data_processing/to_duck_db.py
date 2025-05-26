cea389c5023d7ddea33ce4440d430caf9ad9e111
Muhammad Ihsan <muhihsan0@outlook.com>
2025-05-26 21:14:26 +0700

    testing with pyspark

--- data_processing/to_duck_db.py
+++ data_processing/to_duck_db.py
@@ -2,11 +2,13 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, LongType
 
+# Create Spark Session 
 spark = SparkSession.builder \
         .appName("testing") \
         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
         .getOrCreate()
 
+#
 after_schema = StructType([
     StructField("id", IntegerType()),
     StructField("orders_id", IntegerType()),
